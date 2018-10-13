db86f4c725c51c05f2f107785d7efde5064b35d3
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-12 19:56:00 -0500

    Demo project: trying OpenCV3 + C++

---
+++ demoOpenCV2/cmake-build-debug/CMakeFiles/demoOpenCV2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demoOpenCV2.dir/main.cpp.o"
+  "demoOpenCV2.pdb"
+  "demoOpenCV2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demoOpenCV2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- demoOpenCV2/main.cpp
+++ demoOpenCV2/main.cpp
@@ -1,6 +1,4 @@
 #include "opencv2/opencv.hpp"
-
-
 #include <iostream>
 
 int main() {
